4995c326caa34dfbbc80b38dddb84204ab3bd1b7
Yavuz Asmalı <yavuz.asmali@outlook.com>
2020-07-09 11:55:20 +0300

    Add project files.

---
+++ Authentication/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Yavuz"),
+                new Claim(ClaimTypes.Email, "yavuz@fmail.com"),
+                new Claim("Yavuz.Tells", "Veri nice coder.")
+            };
+
+            var licenceClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Yasmali"),
+                new Claim("Driving Licence", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Yavuz Identity");
+            var licenceIdentity = new ClaimsIdentity(licenceClaims, "Government");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenceIdentity });
+
+            await HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# .NET Core Authentication With AspNetCore.Identity
+
+AspNetCore.Identity creation with EF Core in memory database and authorization pages with AspNetCore Authorization middleware.
+
+* AspNetCore.Identity
+* AspNetCore.Authorization middleware
+* SignInManager async methods
